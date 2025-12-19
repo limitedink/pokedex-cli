e808583b90d19e2a89fa05aa936c6d92539953c1
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-12-19 20:39:54 +1100

    feat: add map and mapb commands for Pokemon location areas
    - Introduce config struct to manage pagination URLs
    - Add locationAreaList and locationAreaItem structs for API response parsing
    - Implement commandMap to fetch and display next 20 location areas
    - Implement commandMapb to fetch and display previous 20 location areas
    - Update commandExit and commandHelp to accept config parameter
    - Add new commands to registry and pass config to callbacks in startRepl

--- repl.go
+++ repl.go
@@ -1,36 +1,121 @@
 package main
-import(
-	"fmt"
-	"strings"
+
+import (
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
-var exitFunc := os.Exit()
+var exitFunc = os.Exit
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func commandExit() error {
+type config struct {
+	NextURL *string
+	PrevURL *string
+}
+
+type locationAreaList struct {
+	Next     *string            `json:"next"`
+	Previous *string            `json:"previous"`
+	Results  []locationAreaItem `json:"results"`
+}
+
+type locationAreaItem struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+func commandExit(cfg *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	exitFunc(0)
 	return nil
 }
 
-func commandHelp() error{
+func commandHelp(cfg *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
-	for _, cmd := range(registry){
+	for _, cmd := range registry {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
 	return nil
-	
 }
 
+func commandMap(cfg *config) error {
+	url := "https://pokeapi.co/api/v2/location-area"
+	if cfg.NextURL != nil {
+		url = *cfg.NextURL
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
 
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var locList locationAreaList
+	err = json.Unmarshal(bodyBytes, &locList)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range locList.Results {
+		fmt.Println(item.Name)
+	}
+
+	cfg.NextURL = locList.Next
+	cfg.PrevURL = locList.Previous
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	url := "https://pokeapi.co/api/v2/location-area"
+	if cfg.PrevURL == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	if cfg.PrevURL != nil {
+		url = *cfg.PrevURL
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	bodyBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var locList locationAreaList
+	err = json.Unmarshal(bodyBytes, &locList)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range locList.Results {
+		fmt.Println(item.Name)
+	}
+
+	cfg.NextURL = locList.Next
+	cfg.PrevURL = locList.Previous
+
+	return nil
+}
 
 var registry = map[string]cliCommand{}
 
@@ -38,28 +123,38 @@ func init() {
 	registry = map[string]cliCommand{
 		"exit": {
 			name:        "exit",
-			description: "Exit the Pokedex",
+			description: "Exit the Pokedex.",
 			callback:    commandExit,
 		},
 		"help": {
 			name:        "help",
-			description: "Prints a help message describing how to use the REPL",
+			description: "Prints a help message describing how to use the REPL.",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays the names of the next 20 location areas in the Pokemon world.",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Like the map command but displays the previous 20 locations/previous page of locations.",
+			callback:    commandMapb,
 		},
 	}
 }
 
-
-func cleanInput (text string) []string{
+func cleanInput(text string) []string {
 	formatted := strings.Fields(strings.TrimSpace(strings.ToLower(text)))
-	return formatted	
+	return formatted
 }
 
-func startRepl () {
+func startRepl() {
+	cfg := &config{}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokédex > ")
-		scanner.Scan()	
+		scanner.Scan()
 		words := cleanInput(scanner.Text())
 		if len(words) == 0 {
 			continue
@@ -67,7 +162,7 @@ func startRepl () {
 		command := words[0]
 		value, exists := registry[command]
 		if exists {
-			err := value.callback()
+			err := value.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -77,5 +172,3 @@ func startRepl () {
 		}
 	}
 }
-
-
